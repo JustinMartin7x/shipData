bb7799b630e07de94a171e4b1a89d1a765a30561
JustinMartin7x <justin.martin7x@gmail.com>
2020-11-03 17:29:50 -0800

    added post put and delete

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -9,56 +9,101 @@ const client = require('../lib/client');
 describe('app routes', () => {
   describe('routes', () => {
     let token;
-  
+
     beforeAll(async done => {
       execSync('npm run setup-db');
-  
+
       client.connect();
-  
+
       const signInData = await fakeRequest(app)
         .post('/auth/signup')
         .send({
           email: 'jon@user.com',
           password: '1234'
         });
-      
+
       token = signInData.body.token;
-  
+
       return done();
     });
-  
+
     afterAll(done => {
       return client.end(done);
     });
 
-  test('returns animals', async() => {
+    test('return rocinante information', async () => {
 
-    const expectation = [
+      const expectation =
       {
-        'id': 1,
-        'name': 'bessie',
-        'coolfactor': 3,
-        'owner_id': 1
-      },
-      {
-        'id': 2,
-        'name': 'jumpy',
-        'coolfactor': 4,
-        'owner_id': 1
-      },
-      {
-        'id': 3,
-        'name': 'spot',
-        'coolfactor': 10,
-        'owner_id': 1
+        id: 1,
+        name: 'Rocinante',
+        weapons: 3,
+        docked: false,
+        size: 'large',
+        class: 'frigate',
+        image: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.reddit.com%2Fr%2Fblender%2Fcomments%2F9b2eii%2Frocinante_from_the_expanse%2F&psig=AOvVaw1mWqWugU8KNOX8PJ3ikpvt&ust=1604452518839000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCPDfq8eZ5ewCFQAAAAAdAAAAABAD'
+
       }
-    ];
+        ;
 
-    const data = await fakeRequest(app)
-      .get('/animals')
-      .expect('Content-Type', /json/)
-      .expect(200);
+      const data = await fakeRequest(app)
+        .get('/ships/1')
+        .expect('Content-Type', /json/)
+        .expect(200);
 
-    expect(data.body).toEqual(expectation);
+      expect(data.body).toEqual(expectation);
+    });
+
+    test('return new ship', async () => {
+
+      const expectation =
+      {
+        id: 3,
+        name: 'ClearWater',
+        weapons: 1,
+        docked: false,
+        size: 'small',
+        class: 'corvette',
+        image: 'random image here'
+
+      }
+        ;
+
+      const data = await fakeRequest(app)
+        .post('/ships/')
+        .expect('Content-Type', /json/)
+        .expect(200);
+
+      expect(data.body).toEqual(expectation);
+
+      const allShips = await fakeRequest(app)
+        .get('/ships/')
+        .expect('Content-Type', /json/)
+        .expect(200);
+      expect(allShips.body.length).toEqual(3);
+
+    });
+    test('delete ship', async () => {
+
+      const expectation =
+      {
+        id: 1,
+        name: 'Rocinante',
+        weapons: 3,
+        docked: false,
+        size: 'large',
+        class: 'frigate',
+        image: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.reddit.com%2Fr%2Fblender%2Fcomments%2F9b2eii%2Frocinante_from_the_expanse%2F&psig=AOvVaw1mWqWugU8KNOX8PJ3ikpvt&ust=1604452518839000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCPDfq8eZ5ewCFQAAAAAdAAAAABAD'
+
+      }
+        ;
+
+      const data = await fakeRequest(app)
+        .delete('/ships/1')
+        .expect('Content-Type', /json/)
+        .expect(200);
+
+      expect(data.body).toEqual(expectation);
+    });
   });
 });
