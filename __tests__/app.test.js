ab6e985e77c2894bac9935dfb2d799b10e41bfd1
JustinMartin7x <justin.martin7x@gmail.com>
2020-11-04 17:05:44 -0800

    commiting

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -40,7 +40,7 @@ describe('app routes', () => {
         weapons: 3,
         docked: false,
         size: 'large',
-        class_id: 'frigate',
+        class_id: '1',
         image: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.reddit.com%2Fr%2Fblender%2Fcomments%2F9b2eii%2Frocinante_from_the_expanse%2F&psig=AOvVaw1mWqWugU8KNOX8PJ3ikpvt&ust=1604452518839000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCPDfq8eZ5ewCFQAAAAAdAAAAABAD',
         owner_id: 1
 
