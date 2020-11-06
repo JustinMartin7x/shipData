227c84e2ff82af8f8eab779d47e907df40794483
JustinMartin7x <justin.martin7x@gmail.com>
2020-11-05 16:12:00 -0800

    bug fixing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -31,7 +31,7 @@ describe('app routes', () => {
       return client.end(done);
     });
 
-    test('return rocinante information', async () => {
+    test.only('return rocinante information', async () => {
 
       const expectation =
       {
@@ -40,7 +40,7 @@ describe('app routes', () => {
         weapons: 3,
         docked: false,
         size: 'large',
-        class_id: '1',
+        class_id: 'frigate',
         image: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.reddit.com%2Fr%2Fblender%2Fcomments%2F9b2eii%2Frocinante_from_the_expanse%2F&psig=AOvVaw1mWqWugU8KNOX8PJ3ikpvt&ust=1604452518839000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCPDfq8eZ5ewCFQAAAAAdAAAAABAD',
         owner_id: 1
 
